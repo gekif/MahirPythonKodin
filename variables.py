76f2af411734c05b19d0bd4d2b1f9a12a454e3a7
FikarFFF <dzulfikar.maulana@gmail.com>
2023-10-13 12:50:37 +0700

    Inisiasi Project

---
+++ variables.py
@@ -0,0 +1,11 @@
+angka = "5"
+angka_berkoma = "3.85"
+nama = "Fajar"
+
+# print(angka)
+# print(angka_berkoma)
+# print(nama)
+
+# Untuk tipe data integer error jika di concat, solusinya tipe data integer diubah ke string
+print(angka + " " + angka_berkoma)
+print("Nama Saya adalah " + nama)
