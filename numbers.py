5f5850c47ee300b36f5065f9b623ad46ca838a24
Gekif <dzulfikar.maulana@gmail.com>
2023-10-14 06:08:43 +0700

    Operator, Operand Arithmetic

---
+++ numbers.py
@@ -0,0 +1,44 @@
+'''
+angka1 = 5
+angka2 = 3
+
+print(angka1 + angka2)
+print("Hasil Penjumlahan: " + str(angka1 + angka2))
+
+print(angka1 % angka2)
+print(2**3)
+'''
+
+'''
+angka = 5
+print("Angka sebelum ditambah: ", angka)
+
+angka += 5
+print("Angka setelah ditambah: ", angka)
+'''
+
+'''
+angka1 = 8
+angka2 = 3
+
+print(angka1 / angka2)
+print(angka1 // angka2)
+'''
+
+'''
+# Function Math pada python
+import math
+
+# Using methods
+print(math.pow(2,3))
+print(math.ceil(3.2))
+print(math.sqrt(25))
+
+# Using Costants
+print(math.pi)
+'''
+
+import math
+
+angka = 4
+print(math.sqrt(angka))
