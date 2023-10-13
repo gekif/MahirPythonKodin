e078743a09b63cfe58c2c95d3ad1e8afac31c052
Gekif <dzulfikar.maulana@gmail.com>
2023-10-13 21:20:28 +0700

    Variable Data Types

--- variables.py
+++ variables.py
@@ -1,11 +1,39 @@
-angka = "5"
-angka_berkoma = "3.85"
-nama = "Fajar"
+angka = 5
+angka_berkoma = 3.85
+
+# angka = "5"
+# angka_berkoma = "3.85"
+
+# nama = "Fajar"
 
 # print(angka)
 # print(angka_berkoma)
 # print(nama)
 
 # Untuk tipe data integer error jika di concat, solusinya tipe data integer diubah ke string
-print(angka + " " + angka_berkoma)
-print("Nama Saya adalah " + nama)
+# print(angka + " " + angka_berkoma)
+
+# Menggunakan type casting
+# print(str(angka) + " " + str(angka_berkoma))
+# print("Nama Saya adalah " + nama)
+
+# print(type(angka))
+# print(type(angka_berkoma))
+# print(type(nama))
+
+
+# Type Casting
+angka_int = 5
+angka_str = str(angka_int)
+angka_float = float(angka_int)
+
+# print(type(angka_int))
+# print(type(angka_str))
+# print(type(angka_float))
+
+
+# Input dan Output
+nama = input("Nama Lengkap: ")
+print("selamat datang, " + nama + "!")
+
+
