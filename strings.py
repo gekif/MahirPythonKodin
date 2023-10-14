9c36f25ddf040e1831ec5695dbedd34ad325c7c0
Gekif <dzulfikar.maulana@gmail.com>
2023-10-14 07:33:29 +0700

    Strings

---
+++ strings.py
@@ -0,0 +1,64 @@
+# nim = "12345678"
+# nama = 'Budud'
+
+# print(nama, "memiliki NIM", nim)
+
+# print("selamat datang bro")
+# print("\"selamat datang bro\"")
+
+
+# Manipulasi String
+'''
+text = "aku pintar"
+
+print(text.upper())
+print(text.lower())
+print(len(text))
+print(text.split(' '))
+print(text.capitalize())
+print(text.index('u'))
+'''
+
+'''
+# Slicing
+
+text = 'Python'
+
+print(text[0])
+print(text[2:])
+print(text[:4])
+print(text[3:5])
+print(text[:])
+print(text[-2])
+print(text[-4:-1])
+'''
+
+'''
+# String Concatenation
+x = "Aku"
+y = "Budi"
+
+print(x, y)
+print(x, "", y)
+print(x, "", y, "", str(100))
+'''
+
+'''
+#  String Format
+
+# print("Akan terjadi error" + 1)
+# print("Akan terjadi error " + str(1))
+
+nama = "cebi"
+umur = 30
+text = "{} berumur {} tahun."
+
+print(text.format(nama, umur))
+'''
+
+
+# In and Not  In
+
+text = 'Assisten Lab'
+print("Assisten" in text)
+print("Aslab" in text)
