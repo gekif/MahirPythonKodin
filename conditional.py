fb031ec3f3de24bbefbfe73338511767967a698a
Gekif <dzulfikar.maulana@gmail.com>
2023-10-14 07:42:36 +0700

    Conditional

---
+++ conditional.py
@@ -0,0 +1,49 @@
+'''
+x = 5
+y = 10
+z = 15
+
+# print (x > y)
+# print (x < y)
+# print (not(x > y))
+
+# print(x < y and y > z)
+# print(x < y or y > z)
+# x < y: 5 < 10: True
+# y > z: 10 > 15: False
+# Hasilnya True and False: False
+# Hasilnya True or False: True
+'''
+
+
+'''
+# Conditional Statements 1
+a = 10
+b = 5
+
+if a != b:
+    print("A tidak sama dengan B")
+'''
+
+'''
+# Conditional Statements 2
+a = 10
+b = 5
+
+if a > b:
+    print("A lebih besar dari B")
+else:
+    print("A tidak lebih besar dari B")
+'''
+
+
+# Conditional Statement 3
+a = 10
+b = 5
+
+if a > b:
+    print("A lebih besar dari B")
+elif a == b:
+    print("A sama dengan B")
+else:
+    print("B lebih besar dari A")
